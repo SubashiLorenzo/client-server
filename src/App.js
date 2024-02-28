3b22724ff6b5e9b8e00bc25b2c971b1f7bc75c33
Antonio Di Marzo <antoniodimarzo95@libero.it>
2024-02-28 12:38:18 +0100

    aggiunti-componenti

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-
+import CreazioneCommento from "./Components/CreazioneCommento";
+import "./App.css";
+import CancellazioneCommenti from "./Components/CancellazioneCommenti";
+import AggiornaCommento from "./Components/AggiornaCommento";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CreazioneCommento></CreazioneCommento>
+      <CancellazioneCommenti></CancellazioneCommenti>
+      <AggiornaCommento></AggiornaCommento>
     </div>
   );
 }
